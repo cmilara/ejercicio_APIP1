9f2ccaebc91b32409cb1a78d001a74b0d6820385
cmilara <cristinamilara@gmail.com>
2023-03-30 17:12:10 +0200

    Primeros pasos

---
+++ src/main/java/com/example/EjercicioApip1Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioApip1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioApip1Application.class, args);
+	}
+
+}
